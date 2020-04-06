25cd910d32bfc990d75a4e9e6c42850ce442d873
shawnmccarthy <shawn@mccarthysoftware.com>
2020-04-06 17:49:45 -0600

    movielist

---
+++ src/components/movie.js
@@ -0,0 +1,22 @@
+import React, { Component }  from 'react';
+import {connect} from "react-redux";
+import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
+import { withRouter } from "react-router-dom";
+import {fetchMovie} from "../actions/movieActions";
+
+//support routing by creating a new component
+
+class Movie extends Component {
+
+}
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps);
+    return {
+        selectedMovie: state.movie.selectedMovie,
+        movieId: ownProps.match.params.movieId
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Movie));
